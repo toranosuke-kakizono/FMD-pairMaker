3badaff774d83ef2ec73f2acc241e14cfcc4a17d
toranosuke kakizono <136287926+toranosuke-kakizono@users.noreply.github.com>
2024-01-23 17:27:36 +0900

    Update script.js
    以下に切り分け
    ・DAY0~20作成するfor文
    ・関数addPairToDate
    ・関数pairMaker
    ・関数randomNumber
    ・関数numbersMaker

--- script.js
+++ script.js
@@ -3,32 +3,78 @@
 //テーブル作成(DAY2~DAY20)
 const pairTable = document.getElementById("pair_table");
 
+//重複のないランダムな配列を作成
+const numbers = numbersMaker(20);
+
+//DAY1~20まで作成
 for (let i = 0; i < 20; i++) {
-    //重複のないランダムな配列を作成
-    const numbers = numbersMaker(20);
+    const th = document.createElement("th");
+    th.id = `DAY${i + 1}`;
+    th.innerText = `DAY${i + 1}`;
 
-    const tr = document.createElement("tr");
-    tr.id = `DAY${i + 1}`;
+    // const DAY = document.createElement("td");
+    // DAY.class = 'DAY';
+    // DAY.innerText = `DAY${i + 1}`;
 
-    const DAY = document.createElement("td");
-    DAY.class = 'DAY';
-    DAY.innerText = `DAY${i + 1}`;
-
-    const pair = document.createElement("td");
-    pair.class = 'pair';
-    pair.innerText = `${numbers[i]}`;
-
-    tr.appendChild(DAY);
-    tr.appendChild(pair);
-    pairTable.appendChild(tr);
+    // th.append(DAY);
+    const DAY1 = document.getElementById("DAY");
+    DAY1.appendChild(th);
 }
 
-//ランダムでペアを作成
+
+addPairToDate(15);
+
+
+//引数の数だけpairMakerでペアを作り、day1~20まで追加！
+/**
+ * 
+ * @param {Number} numberOfPairs ペアの人数
+ */
+function addPairToDate(numberOfPairs) {
+    for (let i = 0; i < 20; i++) {
+        const th_DAY = document.getElementById(`DAY${i + 1}`);
+        const nodeArr = pairMaker(numberOfPairs);
+        // console.log(nodeArr);
+        for (const node of nodeArr) {
+            th_DAY.appendChild(node);
+        }
+    }
+}
+
+/**
+ * 重複がなく、ランダムで入った名前の配列を返す関数
+ * @param {Number} numberOfPairs 
+ * @returns {Array}
+ */
+function pairMaker(numberOfPairs) {
+    //重複のないランダムな配列を作成
+    const numbers = numbersMaker(numberOfPairs);
+    const nodeArr = [];
+    for (let i = 0; i < numberOfPairs; i++) {
+        const td_pair = document.createElement("td");
+        td_pair.class = 'pair';
+        td_pair.id = `pair${i + 1}`;
+        td_pair.innerText = `${numbers[i]}`;
+        td_pair.style.display = "block";
+        nodeArr.push(td_pair);
+    }
+    return nodeArr;
+}
+
+/**
+ * 0~引数までのランダムな数値を作成して返す
+ * @param {Number} num 
+ * @returns {Number}
+ */
 function randomNumber(num) {
     return Math.ceil(Math.random() * num) - 1;
 }
 
-
+/**
+ * 0~引数までの数値を1つずつ不順で配列に格納し、その配列を返す
+ * @param {Number} num 
+ * @returns {Array<Number>}
+ */
 function numbersMaker(num) {
     const numbers = [];
     while (numbers.length < num) {
@@ -37,8 +83,3 @@ function numbersMaker(num) {
     }
     return numbers;
 }
-
-// //randomNumberのテスト
-// for (let i = 0; i < 10; i++) {
-//     console.log(randomNumber(20));
-// }
